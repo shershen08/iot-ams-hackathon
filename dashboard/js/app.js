79eec5f691ba28ab4d87b9e92aaa96c8e57ba5ca
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-04-24 12:58:13 +0200

    working 3 dashboards

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,6 +1,11 @@
 var CHANNELS = [
     'channel_stefan', 'channel_merlando', 'channel_mikhail'
 ];
+var data = [
+    [],
+    [],
+    []
+];
 
 pubnub = PUBNUB({
     publish_key: 'pub-c-b073a796-1ecd-4c4b-8ad8-eddd09773555',
@@ -9,11 +14,24 @@ pubnub = PUBNUB({
 
 CHANNELS.forEach(function(item, index) {
 
+    jQuery('.bot-title:eq(' + index + ')').text(item.split('_')[1]);
+
+    pubnub.subscribe({
+        channel: item,
+        message: function(message, envelope, channelOrGroup, time, channel) {
+
+            if (data[index].length && data[index].length > 60) {
+                data[index] = data[index].slice(1);
+            }
+            if (message.temperature) {
+                data[index].push(message.temperature);
+                $('#' + item).text((message.temperature + '').substr(0, 5) + ' C');
+            }
+        },
+        connect: pub
+    });
 
 })
-
-var data = [];
-var innerCounter = 0;
 
 /**
  * send data from desktop,
@@ -27,41 +45,22 @@ var pushdata = function() {
     });
 }
 
-pubnub.subscribe({
-    channel: CHANNELS[0],
-    message: function(message, envelope, channelOrGroup, time, channel) {
-        /* console.log(
-             "Channel: " + JSON.stringify(channel) + "\n" +
-             "Message: " + JSON.stringify(message) + "\n"
-         );*/
 
-        if (data.length && data.length > 50) {
-            data = data.slice(1);
-        }
-
-        innerCounter++;
-        if (message.temperature) {
-            data.push(message.temperature);
-            $('#channel_stefan').text((message.temperature + '').substr(0, 5) + ' C');
-        }
-    },
-    connect: pub
-});
 
 function pub() {
-    console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
-    pubnub.publish({
-        channel: "channel_stefan",
-        message: { 'temperature': 0 },
-        callback: function(m) { console.log(m) }
-    })
+    // pubnub.publish({
+    //     channel: "channel_stefan",
+    //     message: { 'temperature': 0 },
+    //     callback: function(m) { console.log(m) }
+    // })
+
 }
 
-function getTemparatureData() {
+function getTemperatureData(index) {
 
     var res = [];
-    for (var i = 0; i < data.length; ++i) {
-        res.push([i, data[i]])
+    for (var i = 0; i < data[index].length; ++i) {
+        res.push([i, data[index][i]])
     }
     return res;
 }
@@ -75,9 +74,7 @@ $(document).ready(function($) {
 
 $(function() {
 
-    var container = $("#flot-line-chart-moving");
-
-    series = [{
+    var series = [{
         data: [
             [0, 0]
         ],
@@ -86,57 +83,60 @@ $(function() {
         }
     }];
 
+    $(".flot-line-chart-moving").each(function(index, el) {
 
-    var plot = $.plot(container, series, {
-        grid: {
+        var container = $(this);
+        var plot = $.plot(container, series, {
+            grid: {
 
-            color: "#999999",
-            tickColor: "#D4D4D4",
-            borderWidth: 0,
-            minBorderMargin: 20,
-            labelMargin: 10,
-            backgroundColor: {
-                colors: ["#ffffff", "#ffffff"]
-            },
-            margin: {
-                top: 8,
-                bottom: 20,
-                left: 20
-            },
-            markings: function(axes) {
-                var markings = [];
-                var xaxis = axes.xaxis;
-                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
-                    markings.push({
-                        xaxis: {
-                            from: x,
-                            to: x + xaxis.tickSize
-                        },
-                        color: "#fff"
-                    });
+                color: "#999999",
+                tickColor: "#D4D4D4",
+                borderWidth: 0,
+                minBorderMargin: 20,
+                labelMargin: 10,
+                backgroundColor: {
+                    colors: ["#ffffff", "#ffffff"]
+                },
+                margin: {
+                    top: 8,
+                    bottom: 20,
+                    left: 20
+                },
+                markings: function(axes) {
+                    var markings = [];
+                    var xaxis = axes.xaxis;
+                    for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                        markings.push({
+                            xaxis: {
+                                from: x,
+                                to: x + xaxis.tickSize
+                            },
+                            color: "#fff"
+                        });
+                    }
+                    return markings;
                 }
-                return markings;
+            },
+            colors: ["#1ab394"],
+            xaxis: {
+                tickFormatter: function() {
+                    return "";
+                }
+            },
+            yaxis: {
+                min: 15,
+                max: 35
+            },
+            legend: {
+                show: true
             }
-        },
-        colors: ["#1ab394"],
-        xaxis: {
-            tickFormatter: function() {
-                return "";
-            }
-        },
-        yaxis: {
-            min: 15,
-            max: 35
-        },
-        legend: {
-            show: true
-        }
+        });
+
+        setInterval(function updateRandom() {
+            series[0].data = getTemperatureData(index);
+            plot.setData(series);
+            plot.draw();
+        }, 60); //40
+
     });
-
-    window.e = setInterval(function updateRandom() {
-        series[0].data = getTemparatureData();
-        plot.setData(series);
-        plot.draw();
-    }, 100); //40
-
 });
