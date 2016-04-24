38265e4f2501ca85b0473fdd3a3cf7552e895a96
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-04-24 12:10:45 +0200

    working 1 bot graph

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Folder structure
-``` dashboard ``` - for admin dashboard UI
+``` dashboard ``` - for admin dashboard UI on desktop
+
 ``` main-cp ``` - UI of the main robot control pannel
+
+``` subscribe ``` - code to subsribe for the stream
+
+``` temperature ``` - code for simple robot 
--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,27 +1,49 @@
+var CHANNELS = [
+    'channel_stefan', 'channel_merlando', 'channel_mikhail'
+];
+
 pubnub = PUBNUB({
-    publish_key: 'pub-c-eb8c658b-96ab-43b4-8bb0-b1e04d58230f',
-    subscribe_key: 'sub-c-7427db94-095d-11e6-b422-0619f8945a4f'
+    publish_key: 'pub-c-b073a796-1ecd-4c4b-8ad8-eddd09773555',
+    subscribe_key: 'sub-c-656b4f60-095c-11e6-a5b5-0619f8945a4f'
+});
+
+CHANNELS.forEach(function(item, index) {
+
+
 })
+
+var data = [];
+var innerCounter = 0;
+
+/**
+ * send data from desktop,
+ * just for testing
+ */
 var pushdata = function() {
     pubnub.publish({
-        channel: 'Channel-a7fmfikxg',
-        message: 'Hello from the PubNub Javascript SDK!',
+        channel: 'channel_stefan',
+        message: { 'temperature': 0 },
         callback: function(m) { console.log(m) }
     });
-
 }
 
 pubnub.subscribe({
-    channel: "Channel-a7fmfikxg",
+    channel: CHANNELS[0],
     message: function(message, envelope, channelOrGroup, time, channel) {
-        console.log(
-            "Message Received." + "\n" +
-            "Channel or Group: " + JSON.stringify(channelOrGroup) + "\n" +
-            "Channel: " + JSON.stringify(channel) + "\n" +
-            "Message: " + JSON.stringify(message) + "\n" +
-            "Time: " + time + "\n" +
-            "Raw Envelope: " + JSON.stringify(envelope)
-        )
+        /* console.log(
+             "Channel: " + JSON.stringify(channel) + "\n" +
+             "Message: " + JSON.stringify(message) + "\n"
+         );*/
+
+        if (data.length && data.length > 50) {
+            data = data.slice(1);
+        }
+
+        innerCounter++;
+        if (message.temperature) {
+            data.push(message.temperature);
+            $('#channel_stefan').text((message.temperature + '').substr(0, 5) + ' C');
+        }
     },
     connect: pub
 });
@@ -29,15 +51,92 @@ pubnub.subscribe({
 function pub() {
     console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
     pubnub.publish({
-        channel: "hello_world",
-        message: "Hello from PubNub Docs!",
+        channel: "channel_stefan",
+        message: { 'temperature': 0 },
         callback: function(m) { console.log(m) }
     })
 }
 
+function getTemparatureData() {
+
+    var res = [];
+    for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]])
+    }
+    return res;
+}
 
 $(document).ready(function($) {
     $('#push').click(function(event) {
         pushdata();
     });
 })
+
+
+$(function() {
+
+    var container = $("#flot-line-chart-moving");
+
+    series = [{
+        data: [
+            [0, 0]
+        ],
+        lines: {
+            fill: true
+        }
+    }];
+
+
+    var plot = $.plot(container, series, {
+        grid: {
+
+            color: "#999999",
+            tickColor: "#D4D4D4",
+            borderWidth: 0,
+            minBorderMargin: 20,
+            labelMargin: 10,
+            backgroundColor: {
+                colors: ["#ffffff", "#ffffff"]
+            },
+            margin: {
+                top: 8,
+                bottom: 20,
+                left: 20
+            },
+            markings: function(axes) {
+                var markings = [];
+                var xaxis = axes.xaxis;
+                for (var x = Math.floor(xaxis.min); x < xaxis.max; x += xaxis.tickSize * 2) {
+                    markings.push({
+                        xaxis: {
+                            from: x,
+                            to: x + xaxis.tickSize
+                        },
+                        color: "#fff"
+                    });
+                }
+                return markings;
+            }
+        },
+        colors: ["#1ab394"],
+        xaxis: {
+            tickFormatter: function() {
+                return "";
+            }
+        },
+        yaxis: {
+            min: 15,
+            max: 35
+        },
+        legend: {
+            show: true
+        }
+    });
+
+    window.e = setInterval(function updateRandom() {
+        series[0].data = getTemparatureData();
+        plot.setData(series);
+        plot.draw();
+    }, 100); //40
+
+});
